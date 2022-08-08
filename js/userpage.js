4fbbc5927785aa040e3d75abcd8021c982fc0a3a
Fugujn <109431278+Fugujn@users.noreply.github.com>
2022-08-09 01:01:49 +0700

    add footer and slide

--- js/userpage.js
+++ js/userpage.js
@@ -1,18 +1,52 @@
 function init() {
-    const btnEl = document.querySelector('.btn');
-    const shareEl = document.querySelector('.share');
-    function toggleOptions() {
-        const wrapperEl = shareEl.querySelector('.wrapper');
-        const iconEl = btnEl.querySelector('i');
+    $(document).ready(function () {
+        $(".slide").slick({
+            infinite: true,
+            slidestoShow: 1,
+            slidestoScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            fade: true,
+            cssEase: 'linear',
+            prevArrow: "<button type='button' class='slick-prev slick-arrows'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+            nextArrow: "<button type='button' class='slick-next slick-arrows'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+            responsive: [
+                {
+                    breakpoint: 1201,
+                    settings: {
+                        arrows: false,
+                        draggable: true,
+                        fade: false,
+                    }
+                }
+            ]
+        });
+    });
+    const backToTopbtn = document.querySelector("#back_to_top");
+    window.addEventListener("scroll", scrollFunction);
 
-        wrapperEl.classList.toggle('active');
+    function scrollFunction() {
+        if (window.pageYOffset > 50) {  //show backtotop
+            if (!backToTopbtn.classList.contains("button_entrance")) {
+                backToTopbtn.classList.remove("button_exit");
+                backToTopbtn.classList.add("button_entrance");
 
-        if (iconEl.classList.contains('ri-share-line')) {
-            iconEl.classList.replace('ri-share-line', 'ri-close-line');
-        } else {
-            iconEl.classList.replace('ri-close-line', 'ri-share-line');
+                backToTopbtn.style.display = "block";
+            }
         }
-    };
+        else { //hidden backtotop
+            if (backToTopbtn.classList.contains("button_entrance")) {
+                backToTopbtn.classList.remove("button_entrance");
+                backToTopbtn.classList.add("button_exit");
+                setTimeout(function () {
+                    backToTopbtn.style.display = "none";
+                }, 200);
+            }
+        }
+        backToTopbtn.addEventListener("click", backtotop);
 
-    btnEl.addEventListener('click', toggleOptions);
+        function backtotop() {
+            window.scrollTo(0, 0);
+        }
+    }
 }
