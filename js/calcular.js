15e92e1ac9eaa726cadfee173c77c41f01753c7c
ntrphucvn <trongphuc1321@gmail.com>
2022-08-26 12:46:19 +0700

    ggg

--- js/calcular.js
+++ js/calcular.js
@@ -43,4 +43,32 @@ resultBtn.addEventListener("click", ()=>{
 
 });
 
+ //back to top
+const backToTopbtn =  document.querySelector("#back_to_top");
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+    if (window.pageYOffset > 500) {  //show backtotop
+        if(!backToTopbtn.classList.contains("button_entrance")) {
+            backToTopbtn.classList.remove("button_exit");
+            backToTopbtn.classList.add("button_entrance");
+
+            backToTopbtn.style.display = "block";
+        }
+    }
+    else { //hidden backtotop
+        if (backToTopbtn.classList.contains("button_entrance")) {
+            backToTopbtn.classList.remove("button_entrance");
+            backToTopbtn.classList.add("button_exit");
+            setTimeout( function() {
+                backToTopbtn.style.display = "none";
+            }, 200);
+        }
+    }
+    backToTopbtn.addEventListener("click", backtotop);
+
+    function backtotop() {
+        window.scrollTo(0,0);
+    }
+}
 
